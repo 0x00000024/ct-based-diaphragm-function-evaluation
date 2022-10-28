daa2f76acde27413ced5a9f0982a58fdea3cd38e
Ethan Yu <sohbmpethan@gmail.com>
2022-10-28 17:31:01 +0900

    style(segment): format the hu thresholder

--- src/slice/base.py
+++ src/slice/base.py
@@ -1,8 +1,12 @@
 from typing import List
 import pydicom
 from pydicom import FileDataset
+from nptyping import NDArray, Shape, Int8
+from colorama import Fore
+import cv2 as cv
 from src.slice.hu_thresholder import HounsfieldUnitThresholder
 from src.slice.metadata import Metadata
+from src.slice.denoiser import Denoiser
 
 
 class Slice:
@@ -16,12 +20,25 @@ class Slice:
         self._metadata.change_dicom_file_dataset(self._dicom_file_dataset)
         self._z_value: float = (self._image_index -
                                 1) * self._metadata.slice_thickness
-        print('Image index', self._image_index)
+        print(Fore.CYAN, f'Analyzing image index: {self._image_index}')
 
     def extract_substance_3d_coordinates(self, lower_bound: int,
-                                         upper_bound: int) -> List[List[float]]:
+                                         upper_bound: int,
+                                         roi: str) -> List[List[float]]:
+        denoiser: Denoiser = Denoiser(
+            self._metadata.rows, self._metadata.columns,
+            cv.convertScaleAbs(self._metadata.pixel_data))
+        denoised_data: NDArray[Shape["*, *"], Int8] | None = None
+        if roi == 'thorax':
+            denoised_data: NDArray[Shape["*, *"],
+                                   Int8] = denoiser.set_thorax_as_roi()
+        if roi == 'lung':
+            denoised_data: NDArray[Shape["*, *"],
+                                   Int8] = denoiser.set_lung_as_roi()
+
         hu_thresholder: HounsfieldUnitThresholder = HounsfieldUnitThresholder(
-            lower_bound, upper_bound, self._z_value, self._metadata)
+            lower_bound, upper_bound, self._z_value, self._metadata,
+            denoised_data)
 
         return hu_thresholder.extract_substance_3d_coordinates()
 
--- src/slice/hu_thresholder.py
+++ src/slice/hu_thresholder.py
@@ -1,3 +1,6 @@
+import sys
+import time
+from concurrent.futures import ThreadPoolExecutor, Future, wait, ProcessPoolExecutor
 from typing import List
 import numpy as np
 from nptyping import NDArray, Shape, Int8, Int16
@@ -7,7 +10,8 @@ from src.slice.metadata import Metadata
 class HounsfieldUnitThresholder:
 
     def __init__(self, lower_bound: int, upper_bound: int, z_value,
-                 metadata: Metadata) -> None:
+                 metadata: Metadata, denoised_data: NDArray[Shape["*, *"],
+                                                            Int8]) -> None:
         self._metadata: Metadata = metadata
         self._rows: int = self._metadata.rows
         self._columns: int = self._metadata.columns
@@ -15,16 +19,18 @@ class HounsfieldUnitThresholder:
         self._rescale_intercept = self._metadata.rescale_intercept
         rows: int = self._rows
         columns: int = self._columns
-        self._pixel_data: NDArray[Shape[rows, columns], Int16] = self._metadata.pixel_data
-        self._denoised_data: NDArray[Shape[rows, columns], Int8] = self._metadata.denoised_data
+        self._pixel_data: NDArray[Shape[rows, columns],
+                                  Int16] = self._metadata.pixel_data
+        self._denoised_data: NDArray[Shape[rows, columns], Int8] = denoised_data
         self._lower_bound: int = lower_bound
         self._upper_bound: int = upper_bound
         self._z_value: int = z_value
 
     def extract_substance_3d_coordinates(self) -> List[List[float]]:
-        rows = self._rows
-        columns = self._columns
-        hu_data: NDArray[Shape[rows, columns], Int16] = np.zeros(shape=(rows, columns))
+        rows: int = self._rows
+        columns: int = self._columns
+        hu_data: NDArray[Shape[rows, columns],
+                         Int16] = np.zeros(shape=(rows, columns))
         substance_3d_coordinates: List[List[float]] = []
         step = 1
         for i in range(0, rows, step):
@@ -32,7 +38,8 @@ class HounsfieldUnitThresholder:
                 # Use denoised data as mask
                 if self._denoised_data[i][j] == 0:
                     continue
-                hu_data[i][j] = self._pixel_data[i][j] * self._rescale_slope + self._rescale_intercept
+                hu_data[i][j] = self._pixel_data[i][
+                    j] * self._rescale_slope + self._rescale_intercept
                 if self._lower_bound <= hu_data[i][j] <= self._upper_bound:
                     substance_3d_coordinates.append([
                         i * self._metadata.row_spacing,

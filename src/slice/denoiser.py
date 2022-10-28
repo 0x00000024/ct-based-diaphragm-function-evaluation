5dad040d77fa906a47aa9b0b2b8ec9d8b79309ab
Ethan Yu <sohbmpethan@gmail.com>
2022-10-28 17:30:18 +0900

    feat(denoise): add mask inversion processing and type annotation

--- src/slice/denoiser.py
+++ src/slice/denoiser.py
@@ -2,26 +2,28 @@ import cv2 as cv
 import numpy as np
 from nptyping import NDArray, Shape, Int8
 from src.slice.utility import ClockwiseAngleAndDistance
+from colorama import Fore
 
 
 class Denoiser:
 
     def __init__(self, rows, columns, pixel_data) -> None:
-        self._rows = rows
-        self._columns = columns
+        self._rows: int = rows
+        self._columns: int = columns
         self._pixel_data: NDArray[Shape[rows, columns], Int8] = pixel_data
         kernel_size: int = 13
         self._kernel: NDArray[Shape[kernel_size, kernel_size], Int8] = np.ones(
             (kernel_size, kernel_size), np.uint8)
         self._thorax_mask: NDArray[Shape[rows, columns], Int8] | None = None
-        self.thorax_roi: NDArray[Shape[rows, columns],
-                                 Int8] = self.set_thorax_as_roi()
+        self._thorax_roi: NDArray[Shape[rows, columns], Int8] | None = None
+        self._lung_mask: NDArray[Shape[rows, columns], Int8] | None = None
+        self._lung_roi: NDArray[Shape[rows, columns], Int8] | None = None
 
     def _get_largest_contour_mask(
         self, pixel_data: NDArray[Shape["*, *"],
                                   Int8]) -> NDArray[Shape["*, *"], Int8]:
-        rows = self._rows
-        columns = self._columns
+        rows: int = self._rows
+        columns: int = self._columns
 
         # Use Suzuki's contour tracing algorithm to find the largest contour in the binary image.
         contours: tuple = cv.findContours(image=pixel_data,
@@ -42,9 +44,29 @@ class Denoiser:
 
         return mask
 
+    def _get_inverted_mask(
+        self, pixel_data: NDArray[Shape["*, *"],
+                                  Int8]) -> NDArray[Shape["*, *"], Int8]:
+        rows: int = self._rows
+        columns: int = self._columns
+
+        # Apply erosion morphological transformation with 10 X 10 kernel and 1 iteration.
+        eroded_mask: NDArray[Shape[rows, columns],
+                             Int8] = cv.erode(src=pixel_data,
+                                              kernel=self._kernel,
+                                              iterations=1)
+
+        # Apply an inverse binary thresholding to the mask.
+        _, inverted_mask = cv.threshold(src=eroded_mask,
+                                        thresh=127,
+                                        maxval=255,
+                                        type=cv.THRESH_BINARY_INV)
+
+        return inverted_mask
+
     def set_thorax_as_roi(self) -> NDArray[Shape["*, *"], Int8]:
-        rows = self._rows
-        columns = self._columns
+        rows: int = self._rows
+        columns: int = self._columns
 
         # Apply binary thresholding,
         # if pixel intensity is greater than the 127, value set to 255 (white), else set to 0 (black).
@@ -53,44 +75,39 @@ class Denoiser:
                                  maxval=255,
                                  type=cv.THRESH_BINARY)
 
-        # Smooth the largest contour
+        # Smooth the largest contour.
         smoothed_image: NDArray[Shape[rows, columns],
                                 Int8] = cv.morphologyEx(src=thresh,
                                                         op=cv.MORPH_OPEN,
                                                         kernel=self._kernel)
 
-        # Get largest contour mask
+        # Get largest contour mask.
         self._thorax_mask = self._get_largest_contour_mask(smoothed_image)
 
         # Use bitwise and operation and mask to construct ROI.
         return cv.bitwise_and(self._pixel_data, self._thorax_mask)
 
-    def set_lungs_as_roi(self) -> NDArray[Shape["*, *"], Int8]:
-        rows = self._rows
-        columns = self._columns
+    def set_lung_as_roi(self) -> NDArray[Shape["*, *"], Int8]:
+        if self._thorax_roi is None:
+            self._thorax_roi = self.set_thorax_as_roi()
 
-        # Apply erosion morphological transformation with 10 X 10 kernel and 1 iteration
-        eroded_mask: NDArray[Shape[rows, columns],
-                             Int8] = cv.erode(src=self._thorax_mask,
-                                              kernel=self._kernel,
-                                              iterations=1)
-
-        # Apply an inverse binary thresholding to the mask to remove the black color on the outer circle of the thorax.
-        _, inverse_mask = cv.threshold(src=eroded_mask,
-                                       thresh=127,
-                                       maxval=255,
-                                       type=cv.THRESH_BINARY_INV)
+        rows: int = self._rows
+        columns: int = self._columns
 
         # Apply binary thresholding,
         # if the pixel intensity is greater than 127, set the value to 255 (white), otherwise set to 0 (black).
-        _, thresh = cv.threshold(src=self.thorax_roi,
+        _, thresh = cv.threshold(src=self._thorax_roi,
                                  thresh=127,
                                  maxval=255,
                                  type=cv.THRESH_BINARY)
 
+        # Get the inverted mask used to extract the lung area.
+        thorax_inverted_mask = self._get_inverted_mask(self._thorax_mask)
+
         # Use bitwise or operation and inverse mask to extract the lung area.
         lung_roi: NDArray[Shape[rows, columns],
-                          Int8] = cv.bitwise_or(src1=thresh, src2=inverse_mask)
+                          Int8] = cv.bitwise_or(src1=thresh,
+                                                src2=thorax_inverted_mask)
 
         # Apply the inverse binary thresholding to change the pixel value of the lung area to 255 (white).
         _, lung_roi = cv.threshold(src=lung_roi,
@@ -98,11 +115,11 @@ class Denoiser:
                                    maxval=255,
                                    type=cv.THRESH_BINARY_INV)
 
-        # No lungs detected, return full black mask
+        # If no lungs are detected, return to full black mask.
         if (lung_roi == 0).all():
             return lung_roi
 
-        # Smooth the largest contour
+        # Smooth the largest contour.
         smoothed_image: NDArray[Shape[rows, columns],
                                 Int8] = cv.morphologyEx(src=lung_roi,
                                                         op=cv.MORPH_CLOSE,
@@ -124,6 +141,7 @@ class Denoiser:
                             raise Exception
                     except Exception as e:
                         print(
+                            Fore.YELLOW,
                             f'Points with more than two coordinates: {pts}, {e}'
                         )
 
@@ -147,6 +165,24 @@ class Denoiser:
         lung_mask: NDArray[Shape[rows, columns],
                            Int8] = self._get_largest_contour_mask(
                                smoothed_image)
-        # lung_mask = cv.dilate(src=lung_mask, kernel=self._kernel, iterations=1)
 
-        return cv.bitwise_and(self._pixel_data, lung_mask)
+        # Enlarge the mask of the lungs to preserve the lung boundaries.
+        self._lung_mask = cv.dilate(src=lung_mask,
+                                    kernel=self._kernel,
+                                    iterations=1)
+
+        return cv.bitwise_and(self._pixel_data, self._lung_mask)
+
+    def set_heart_as_roi(self) -> NDArray[Shape["*, *"], Int8]:
+        if self._lung_roi is None:
+            self._lung_roi = self.set_lung_as_roi()
+
+        rows: int = self._rows
+        columns: int = self._columns
+
+        # Get the inverted mask used to extract non-lung areas in the thorax.
+        lung_inverted_mask: NDArray[Shape[rows, columns],
+                                    Int8] = self._get_inverted_mask(
+                                        self._lung_mask)
+
+        return cv.bitwise_and(self._thorax_mask, lung_inverted_mask)
